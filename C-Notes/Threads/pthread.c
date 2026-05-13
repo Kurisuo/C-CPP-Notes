447b524b0957929f0306711fa40a635b4113828b
Christopher Zarraga <christopherzarraga@eduroam-169-233-218-36.ucsc.edu>
2026-05-13 14:26:14 -0700

    Added Beej's C Guide and reviewed pointers

--- C-Notes/Threads/pthread.c
+++ C-Notes/Threads/pthread.c
@@ -6,17 +6,26 @@
 
 void* roll_dice(){ 
     int value = (rand() % 6) + 1;
-    printf("%d\n", value);
-    return NULL;
+    int* result = malloc(sizeof(int));
+    *result = value;
+    return (void*) result;
 }
 
-
-
-
 int main(int argc, char* argv[]){ 
+    int* res;
     srand(time(NULL)); //!NEW!
     pthread_t thread_1;
-    
+    if(pthread_create(&thread_1, NULL, &roll_dice, NULL) != 0){ 
+        return 1;
+    }
 
+    if(pthread_join(thread_1, (void**) &res) != 0){
+        return 2;
+    }
+
+    printf("Main res: %p\n", res);
+    printf("Results: %d\n", res);
+    
+    free(res);
     return 0;
 }
